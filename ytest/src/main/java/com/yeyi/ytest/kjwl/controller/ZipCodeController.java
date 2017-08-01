f8bd9d38d7ef5d1f9cc09b5ae086e6be1ec84cdb
yeyi771 <yeyi771@overLenovo>
2017-08-01 09:43:24 +0800

    增加例子

---
+++ ytest/src/main/java/com/yeyi/ytest/kjwl/controller/ZipCodeController.java
@@ -0,0 +1,10 @@
+package com.yeyi.ytest.kjwl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/kjwl")
+public class ZipCodeController {
+
+}
---
+++ ytest/src/main/java/com/yeyi/ytest/kjwl/service/ZipCodeService.java
@@ -0,0 +1,8 @@
+package com.yeyi.ytest.kjwl.service;
+
+import org.springframework.stereotype.Service;
+
+@Service // 将此类标记为一个服务层的 bean
+public class ZipCodeService {
+
+}
